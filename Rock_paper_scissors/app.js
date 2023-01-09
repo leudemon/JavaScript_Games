a9b66611463129896fab47db5811bf103d8f6e1f
leudemon <avit.mugisha@gmail.com>
2023-01-09 23:28:16 +0200

    upgrades

--- Rock_paper_scissors/app.js
+++ Rock_paper_scissors/app.js
@@ -2,43 +2,29 @@ const computerChoiceDisplay = document.getElementById("computerChoice")
 const userChoiceDisplay = document.getElementById("userChoice")
 const resultDisplay = document.getElementById("result")
 const choices = document.querySelectorAll("button")
+const gif1 = document.createElement("img")//delete if not used
+const mainBackground = document.getElementById("griditem1")
+const background = document.getElementById('main-body')
+
+
 
 let user_choice
 let computer_choice
 let gotresult
-let counter
-let winner = ''
+
+background.style.backgroundImage = "none"
 
 let scores = {
     pc: 0,
     user: 0,
-    cal: (pc, user)=>{
+    cal: (pc, user) => {
         this.pc = pc
         this.user = user
 
-      
-        if(this.pc === 10 || this.user === 10){
-            counter = "done"
-        }
-        while (counter !== "done"){
-            if (this.pc > this.user) {
-                return "Losing..."
-            }
-            else if(this.pc < this.user) {return "Winning!"}
-            else if (this.user === 10) {
-                winner = "user"
-                counter = "done"
-                return  winner
-            }else if(this.pc === 10){
-                winner = "computer"
-                counter = "done"
-                return winner
-            }
-        }
     }
 }
 
-choices.forEach(choice => choice.addEventListener("click", (e)=>{
+choices.forEach(choice => choice.addEventListener("click", (e) => {
     user_choice = e.target.id
     userChoiceDisplay.innerHTML = user_choice
     computer_choice = getRandomChoice()
@@ -47,12 +33,14 @@ choices.forEach(choice => choice.addEventListener("click", (e)=>{
     resultDisplay.innerHTML = gotresult
     console.log(gotresult)
     console.log(getScores(scores))
-    console.log(scores.pc + " | " + scores.user )
+    console.log("pc: " + scores.pc + " | " + "user :" + scores.user)
+    
+
 
 }))
 
-const getRandomChoice = ()=>{
-    let randomNumber = Math.floor(Math.random()*choices.length) + 1
+const getRandomChoice = () => {
+    let randomNumber = Math.floor(Math.random() * choices.length) + 1
     switch (randomNumber) {
         case 1:
             return "Rock"
@@ -65,16 +53,16 @@ const getRandomChoice = ()=>{
     }
 }
 
-const getResult = (user_choice, computer_choice)=>{
+const getResult = (user_choice, computer_choice) => {
 
-    if (user_choice === computer_choice){ return "Draw!"} 
-    if((user_choice === "Rock") && (computer_choice === "Paper")) {return "You Lost!"}
-    if((user_choice === "Rock") && (computer_choice === "Scissors")){return "You Won!"} 
-    if((user_choice === "Scissors") && (computer_choice === "Rock")){ return "You Lost!"}
-    if((user_choice === "Paper") && (computer_choice === "Rock")) {return "You Won!"}
-    if((user_choice === "Paper") && (computer_choice === "Scissors")){return "You Lost!"}
-    if((user_choice === "Scissors") && (computer_choice === "Paper")){return "You Won!"}
-    else{
+    if (user_choice === computer_choice) { return "Draw!" }
+    if ((user_choice === "Rock") && (computer_choice === "Paper")) { return "You Lost!" }
+    if ((user_choice === "Rock") && (computer_choice === "Scissors")) { return "You Won!" }
+    if ((user_choice === "Scissors") && (computer_choice === "Rock")) { return "You Lost!" }
+    if ((user_choice === "Paper") && (computer_choice === "Rock")) { return "You Won!" }
+    if ((user_choice === "Paper") && (computer_choice === "Scissors")) { return "You Lost!" }
+    if ((user_choice === "Scissors") && (computer_choice === "Paper")) { return "You Won!" }
+    else {
         return undefined
     }
 
@@ -86,11 +74,25 @@ const getResult = (user_choice, computer_choice)=>{
 const getScores = (scores) => {
     switch (gotresult) {
         case "You Lost!":
-            scores.pc ++
+            scores.pc++
+            mainBackground.style.backgroundColor = "#ff0000"
+            break
         case "You Won!":
             scores.user++
+            mainBackground.style.backgroundColor = "lightgreen"
+            break
         default:
+            mainBackground.style.backgroundColor = "white"
             break;
     }
-    return scores.cal(this.pc, this.user)
+    if (scores.pc === 5) {
+        alert("That's a Loss! try again?")
+        location.reload()
+    }
+    if (scores.user === 5) {
+
+
+        alert("Good Job, You Won!")
+        location.reload()
+    }
 }
