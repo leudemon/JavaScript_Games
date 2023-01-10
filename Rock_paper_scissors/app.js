536b2e8a09509cdd67e62cc14c7eabeb12ab78d2
leudemon <avit.mugisha@gmail.com>
2023-01-10 19:09:12 +0200

    customized buttons and added emojies

--- Rock_paper_scissors/app.js
+++ Rock_paper_scissors/app.js
@@ -1,30 +1,63 @@
-const computerChoiceDisplay = document.getElementById("computerChoice")
-const userChoiceDisplay = document.getElementById("userChoice")
-const resultDisplay = document.getElementById("result")
-const choices = document.querySelectorAll("button")
-const gif1 = document.createElement("img")//delete if not used
-const mainBackground = document.getElementById("griditem1")
-const background = document.getElementById('main-body')
-
-
-
 let user_choice
 let computer_choice
 let gotresult
 let chosenItem_user
 let chosenItem_pc
+let winner
+let playground 
+let background 
 
+const computerChoiceDisplay = document.getElementById("computerChoice")
+const userChoiceDisplay = document.getElementById("userChoice")
+const resultDisplay = document.getElementById("result")
+const choices = document.querySelectorAll("button")
+const mainBackground = document.getElementById("main-body")
+const buttonOn = document.addEventListener("onclick",()=>console.log("button on"))
+const buttonOff = document.addEventListener("Mouse leave", () => console.log("button off"))
+const versus = document.getElementById("versus")
+const subVersus = document.getElementById("ver")
 const imgRock = '<img src="/assets/rock.png">'
 const imgPaper = '<img src="/assets/paper.png">'
-const imgScissors= '<img src="/assets/scissors.png">'
+const imgScissors = '<img src="/assets/scissors.png">'
+
+mainBackground.style.backgroundImage = "none"
+
+
+window.onload = ()=>{
+    versus.classList.replace("show", "hide")
+    console.log("hey")
+}
 
 const items = {
     Rock: imgRock,
     Paper: imgPaper,
     Scissors: imgScissors
 }
+const sad_emojies = {
+    1: '😪',
+    2: '😥',
+    3: '😤',
+    4: '😫',
+    5: '😓',
+    6: '😢',
+    7: '😭',
+    8: '😖',
+    9: '😬'
+}
+const happy_emojies = {
+    1: '😋',
+    2: '🤗',
+    3: '😏',
+    4: '😎',
+    5: '😊',
+    6: '😚',
+    7: '😍',
+    8: '🥰',
+    9: '🙂'
+}
 
-background.style.backgroundImage = "none"
+
+
 
 let scores = {
     pc: 0,
@@ -36,17 +69,23 @@ let scores = {
     }
 }
 
+
+
 choices.forEach(choice => choice.addEventListener("click", (e) => {
-    
+    background = document.getElementById(e.target.id)
+
+    //after window loads show versus
+    versus.classList.replace("hide", "show")
+    subVersus.classList.replace("show", "hide")
     //User's choice
     user_choice = e.target.id
     chosenItem_user = items[user_choice]
     console.log(chosenItem_user);
     userChoiceDisplay.innerHTML = chosenItem_user
-    
+
     //Computer's choice
     computer_choice = getRandomChoice()
-    
+
     chosenItem_pc = items[computer_choice]
     console.log(computer_choice)
     computerChoiceDisplay.innerHTML = chosenItem_pc
@@ -54,26 +93,26 @@ choices.forEach(choice => choice.addEventListener("click", (e) => {
     //Display results
     gotresult = getResult(user_choice, computer_choice)
     resultDisplay.innerHTML = gotresult
-    console.log(gotresult)
     getScores(scores)
     console.log("pc: " + scores.pc + " | " + "user :" + scores.user)
-    
+
 
 
 }))
 
 
-getKeyByValue = (object, value)=>{
-    for (let prop in object){
-        if (object.hasOwnProperty(prop)){
+getKeyByValue = (object, value) => {
+    for (let prop in object) {
+        if (object.hasOwnProperty(prop)) {
             if (object[prop] === value)
-            return prop
+                return prop
         }
     }
 }
 
+const getRandomValue = values => Math.floor(Math.random() * values.length) + 1
 const getRandomChoice = () => {
-    let randomNumber = Math.floor(Math.random() * choices.length) + 1
+    let randomNumber = getRandomValue(choices)
     switch (randomNumber) {
         case 1:
             return "Rock"
@@ -87,44 +126,81 @@ const getRandomChoice = () => {
 }
 
 const getResult = (user_choice, computer_choice) => {
-
-    if (user_choice === computer_choice) { return "Draw!" }
-    if ((user_choice === "Rock") && (computer_choice === "Paper")) { return "You Lost!" }
-    if ((user_choice === "Rock") && (computer_choice === "Scissors")) { return "You Won!" }
-    if ((user_choice === "Scissors") && (computer_choice === "Rock")) { return "You Lost!" }
-    if ((user_choice === "Paper") && (computer_choice === "Rock")) { return "You Won!" }
-    if ((user_choice === "Paper") && (computer_choice === "Scissors")) { return "You Lost!" }
-    if ((user_choice === "Scissors") && (computer_choice === "Paper")) { return "You Won!" }
+    let sad = getRandomValue(Object.keys(sad_emojies))
+    let happy = getRandomValue(Object.keys(happy_emojies))
+    if (user_choice === computer_choice) { 
+        winner = "" 
+        return "😐" }
+    if ((user_choice === "Rock") && (computer_choice === "Paper")) { 
+        winner = "pc"
+        return sad_emojies[sad] }
+    if ((user_choice === "Rock") && (computer_choice === "Scissors")) { 
+        winner = "user"
+        return happy_emojies[happy] }
+    if ((user_choice === "Scissors") && (computer_choice === "Rock")) { 
+        winner = "pc"
+        return sad_emojies[sad] }
+    if ((user_choice === "Paper") && (computer_choice === "Rock")) { 
+        winner = "user"
+        return happy_emojies[happy] }
+    if ((user_choice === "Paper") && (computer_choice === "Scissors")) { 
+        winner = "pc"
+        return sad_emojies[sad] }
+    if ((user_choice === "Scissors") && (computer_choice === "Paper")) { 
+        winner = "user"
+        return happy_emojies[happy] }
     else {
         return undefined
     }
 
 }
 
+const button_color_change = ()=>{
+    if (winner === "user"){
+        background.style.backgroundColor = "#00ff00"
+    }else if (winner === "pc"){
+        background.style.backgroundColor = "#ff0000"
+    }
+    else{
+        background.style.backgroundColor = "#ffffff"
+    }
+}
+
+const button_color_reset = () => {
+    // background.style.backgroundColor = "#ffffff"
+}
+
+
 
 
 
 const getScores = (scores) => {
-    switch (gotresult) {
-        case "You Lost!":
+    playground = document.getElementById("griditem1")
+    switch (winner) {
+        case "pc":
             scores.pc++
-            mainBackground.style.backgroundColor = "#ff0000"
+            playground.style.backgroundColor = "#ff0000"
+            
             break
-        case "You Won!":
+        case "user":
             scores.user++
-            mainBackground.style.backgroundColor = "lightgreen"
+            playground.style.backgroundColor = "#00ff00"
+            
             break
         default:
-            mainBackground.style.backgroundColor = "white"
+            playground.style.backgroundColor = "#ffffff"
             break;
     }
+
     if (scores.pc === 5) {
+        scores.pc = 0
+        scores.user = 0
         alert("That's a Loss! try again?")
         location.reload()
     }
     if (scores.user === 5) {
-
-
+        scores.pc = 0
+        scores.user = 0
         alert("Good Job, You Won!")
         location.reload()
     }
